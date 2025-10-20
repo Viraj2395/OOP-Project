0aa1ca5f7314da12ff1e6fca5dbfdbb3f7288426
DakshBansalU <daksh.k.bansal@gmail.com>
2025-10-20 15:54:22 +1030

    changes

---
+++ build/libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/DependInfo.cmake
@@ -0,0 +1,59 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/axes_objects/bars.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/bars.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/bars.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/axes_objects/box_chart.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/box_chart.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/box_chart.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/axes_objects/circles.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/circles.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/circles.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/axes_objects/contours.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/contours.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/contours.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/axes_objects/error_bar.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/error_bar.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/error_bar.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/axes_objects/filled_area.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/filled_area.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/filled_area.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/axes_objects/function_line.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/function_line.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/function_line.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/axes_objects/histogram.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/histogram.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/histogram.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/axes_objects/labels.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/labels.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/labels.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/axes_objects/line.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/line.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/line.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/axes_objects/matrix.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/matrix.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/matrix.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/axes_objects/network.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/network.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/network.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/axes_objects/parallel_lines.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/parallel_lines.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/parallel_lines.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/axes_objects/stair.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/stair.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/stair.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/axes_objects/string_function.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/string_function.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/string_function.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/axes_objects/surface.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/surface.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/surface.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/axes_objects/vectors.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/vectors.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/axes_objects/vectors.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/backend/backend_interface.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/backend/backend_interface.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/backend/backend_interface.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/backend/backend_registry.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/backend/backend_registry.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/backend/backend_registry.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/backend/gnuplot.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/backend/gnuplot.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/backend/gnuplot.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/core/axes_object.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/core/axes_object.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/core/axes_object.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/core/axes_type.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/core/axes_type.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/core/axes_type.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/core/axis_type.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/core/axis_type.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/core/axis_type.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/core/figure_registry.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/core/figure_registry.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/core/figure_registry.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/core/figure_type.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/core/figure_type.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/core/figure_type.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/core/legend.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/core/legend.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/core/legend.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/core/line_spec.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/core/line_spec.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/core/line_spec.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/freestanding/axes_functions.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/freestanding/axes_functions.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/freestanding/axes_functions.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/freestanding/histcounts.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/freestanding/histcounts.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/freestanding/histcounts.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/util/colors.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/util/colors.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/util/colors.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/util/common.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/util/common.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/util/common.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/util/contourc.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/util/contourc.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/util/contourc.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/util/popen.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/util/popen.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/util/popen.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/util/world_cities.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/util/world_cities.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/util/world_cities.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/util/world_map_10m.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/util/world_map_10m.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/util/world_map_10m.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/util/world_map_110m.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/util/world_map_110m.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/util/world_map_110m.cpp.o.d"
+  "/home/daksh/OOP-Project/libs/matplotplusplus/source/matplot/util/world_map_50m.cpp" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/util/world_map_50m.cpp.o" "gcc" "libs/matplotplusplus/source/matplot/CMakeFiles/matplot.dir/util/world_map_50m.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
